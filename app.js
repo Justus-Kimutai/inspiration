8bfc0dc0c825df55a62fd27d0b9c836144b17b32
Justus-Kimutai <justuskiprotich01@gmail.com>
2023-09-02 23:24:20 +0300

    adjusted the main Text

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let textDiv = document.querySelector('.text')
-const stringText = 'Hello There, kimutai here wishing you a goodnight'
+const stringText = 'Hello There, kimutai here wishing you a fruitful month full of Gods grace and favour, may all that the enemy intended to harm you work for your good in this september in Jesus name. Good night   #stay Motivated'
 
 const arrString = stringText.split("")
 
@@ -13,5 +13,5 @@ let timeOut = setInterval(() => {
     if(index === arrString.length){
         clearInterval(timeOut)
     }
-}, 200);
+}, 100);
 
